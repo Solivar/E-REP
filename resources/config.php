8b0a296767ec0f64977919ccd2a2a916215317ab
unknown <krikus6@inbox.lv>
2014-07-18 11:51:21 +0300

    debugged

--- resources/config.php
+++ resources/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqlhost = 			"127.0.0.1"; // MYSQL HOST, secure default is 127.0.0.1, otherwise use locahost but it can break the script.
+$mysqlhost = 			"192.168.11.11"; // MYSQL HOST, secure default is 127.0.0.1, otherwise use locahost but it can break the script.
 $mysqluser = 			"root"; // MYSQL database username
 $mysqlpassword = 	"root"; // MYSQL database password if  you have set one
 $mysqldatabase = 	"EREP"; // MYSQL database name
