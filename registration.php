0c6b3085e7bcdda5da0b136b001cb25f55b30645
Solivar <solovjovs.ivars@gmail.com>
2014-07-18 11:26:50 +0300

    Registartion form styling

--- registration.php
+++ registration.php
@@ -3,13 +3,15 @@
 <?php include 'header.php'; ?>
 <body>
 <?php include 'navigation.php'; ?>
-<form ACTION="register.php" METHOD=post>
-<p>Username:</p><input name="regname" type="text" size"20"></input>
-<p>E-mail:</p><input name="email" type="text" size"20"></input>
-<p>Password:</p><input name="regpass1" type="password" size"20"></input>
-<p>Repeat Password:</p><input name="regpass2" type="password" size"20"></input>
-<input type="submit" />
-</form>
+<div class="registration-form">
+	<form ACTION="register.php" METHOD=post>
+		<p>Username:</p><input name="regname" type="text" size="20"></input>
+		<p>E-mail:</p><input name="email" type="text" size="20"></input>
+		<p>Password:</p><input name="regpass1" type="password" size="20"></input>
+		<p>Repeat Password:</p><input name="regpass2" type="password" size="20"></input>
+		<input type="submit" />
+	</form>
+</div>
 <?php include 'footer.php'; ?>
 </body>
 </html>
