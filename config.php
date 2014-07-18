c70beb9a677a9ba8619e8d0b48e0f933b4c4fc5e
Solivar <solovjovs.ivars@gmail.com>
2014-07-18 11:07:11 +0300

    Deleted some files and did more structuring

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mysqlhost = 			"127.0.0.1"; // MYSQL HOST, secure default is 127.0.0.1, otherwise use locahost but it can break the script.
-$mysqluser = 			"root"; // MYSQL database username
-$mysqlpassword = 	"root"; // MYSQL database password if  you have set one
-$mysqldatabase = 	"EREP"; // MYSQL database name
-$mysqluser = 			"user"; //Main table user, do not change this
-?>
