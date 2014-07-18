31738098ac5a8c2f90a722f14891b459dcce760d
Solivar <solovjovs.ivars@gmail.com>
2014-07-18 10:21:52 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,4 @@
+<head>
+<title>E-REP || Virtual reputation tracking</title>
+<link rel="stylesheet" type="text/css" href="css/erep.css" />
+</head>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<?php
+include header.php;
+?>
+<body>
+<p>Hello there</p>
+<?php
+include footer.php;
+?>
+</body>
+</html>
