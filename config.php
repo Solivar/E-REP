a0964d8fb4374cc089b46ad4f8bb73ce1ec5beb0
unknown <Hetachi>
2014-07-18 10:18:26 +0300

    Added MYSQL config file for all scripts

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$mysqlhost = 			"127.0.0.1"; // MYSQL HOST, secure default is 127.0.0.1, otherwise use locahost but it can break the script.
+$mysqluser = 			"root"; // MYSQL database username
+$mysqlpassword = 	""; // MYSQL database password if  you have set one
+$mysqldatbase = 		"EREP"; // MYSQL database name
+
+?>
