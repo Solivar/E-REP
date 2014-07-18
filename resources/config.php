4848c13e68eabb62ba2a687400e2b61c37680417
unknown <krikus6@inbox.lv>
2014-07-18 12:07:26 +0300

    Ip changes

--- resources/config.php
+++ resources/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$mysqlhost = 			"192.168.11.11"; // MYSQL HOST, secure default is 127.0.0.1, otherwise use locahost but it can break the script.
+$mysqlhost = 			"127.0.0.1"; // MYSQL HOST, secure default is 127.0.0.1, otherwise use locahost but it can break the script.
 $mysqluser = 			"root"; // MYSQL database username
-$mysqlpassword = 	"root"; // MYSQL database password if  you have set one
+$mysqlpassword = 	""; // MYSQL database password if  you have set one
 $mysqldatabase = 	"EREP"; // MYSQL database name
 $mysqluser = 			"user"; //Main table user, do not change this
 ?>
