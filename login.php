19db1dd423b14dc0556d290e8f272be501e3dc0c
unknown <krikus6@inbox.lv>
2014-07-20 00:43:28 +0300

    Added login system and login check, now sets on login sets session $myusername

---
+++ login.php
@@ -0,0 +1,17 @@
+
+
+
+
+<form name="form1" method="post" action="./resources/functions/checklogin.php">
+	
+	<p>Username:</p>
+
+	<input name="myusername" type="text" id="myusername"></td>
+
+	<p>Password: </p>
+
+	<input name="mypassword" type="password" id="mypassword"></td>
+		</br>
+		</br>
+	<button id="reg" type="submit" name="Submit" value="Login">Login</button>
+</form>
