d456e6c79a8cd7daa272cf977746c777fee7b35b
unknown <krikus6@inbox.lv>
2014-07-20 14:26:32 +0300

    Updated registration SQL injection safety

---
+++ navigation.php
@@ -0,0 +1,6 @@
+<div class="navigation">
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="registration.php">Registration</a></li>
+	</ul>
+</div>
--- resources/functions/register.php
+++ resources/functions/register.php
@@ -2,6 +2,14 @@
 	session_start();
 	$salt = "ripemd128";
 	$con = mysqli_connect('127.0.0.1', 'root', '', 'erep');
+	$_POST['regname'] = stripslashes($_POST['regname']);
+	$_POST['regpass1'] = stripslashes($_POST['regpass1']);
+	$_POST['regpass2'] = stripslashes($_POST['regpass2']);
+	$_POST['email'] = stripslashes($_POST['email']);
+	$_POST['regname'] = $con->real_escape_string($_POST['regname']);
+	$_POST['regpass1'] = $con->real_escape_string($_POST['regpass1']);
+	$_POST['regpass2'] = $con->real_escape_string($_POST['regpass2']);
+	$_POST['email'] = $con->real_escape_string($_POST['email']);
 	if (isSet($_POST['reg']) && isSet($_POST['regname']) && isSet($_POST['regpass1']) && $_POST['regname'] != '' && $_POST['regpass1'] != '' && $_POST['regpass1'] == $_POST['regpass2']) {
 		$regpass1 = $_POST['regpass1'];
 		$regpass1MD5 = hash($salt,$_POST['regpass1']);
