baa1cff801cc335aaf305660987ae6b83f2dc74c
Solivar <solovjovs.ivars@gmail.com>
2014-07-20 16:25:13 +0300

    Member-list

--- app.php
+++ app.php
@@ -4,16 +4,9 @@
 <body>
 <div id="container">
 	<div class="content">
-	<p>Lorem ipsum dolor sit amet, te has habeo appellantur, stet eripuit assentior vim eu. Ne populo verear conceptam pro, est sale soluta ex. Te mentitum adversarium sit, et hinc delicata quo, eos te modus inani. Sit ad pertinax petentium. His id fabellas theophrastus consequuntur, no audiam voluptaria usu, autem soleat similique quo in. Ea vel oblique admodum, mea ne saepe omnesque gubergren, veritus insolens sed ne.
-
-	Denique vituperatoribus id has. Eos ea bonorum splendide. Vis homero urbanitas ne. Eum ex electram vulputate definitionem, in pri tollit soluta.
-	</p>
-	<br />
-	<p>
-	Lorem ipsum dolor sit amet, diceret minimum mnesarchum vim id. Vis te augue movet maiestatis. Nam scripta erroribus vulputate cu, eu integre signiferumque per. Eu vel lorem persius, illud detracto adversarium at eos, per ut idque propriae laboramus.
-
-Mel libris albucius scaevola ne, volutpat salutatus patrioque vel et. Diam dicunt pericula has ex, appetere intellegebat vix an. Viderer scribentur est id. Ad etiam omnesque sententiae cum, docendi commune antiopam est ea, eum in novum doming accusamus. Cu per tollit euripidis splendide, sea dolore noster id. Pri ut nullam dolores consectetuer, et eos discere dolorem.
-	</p>
+		<p>This is the application page</p>
+			<p>Welcome <?php echo $_SESSION['myusername']; ?></p>
+		<p><a href="">My Profile</a></p>
 	</div>
 </div>
 <?php include 'footer.php'; ?>
