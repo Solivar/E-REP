c24d2fd04b14a84c70d28fbb0f110b098ddaefc6
Hetachi <krikus6@inbox.lv>
2014-10-05 18:07:53 +0300

    New version structure setup.

---
+++ resources/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 			"127.0.0.1"; // MYSQL HOST, secure default is 127.0.0.1, otherwise use locahost but it can break the script.
+$username =		"root"; // MYSQL database username
+$password = 	""; // MYSQL database password if  you have set one
+$db_name = 		"EREP"; // MYSQL database name
+$table_name = 	"user"; //Main table user, do not change this
+?>
