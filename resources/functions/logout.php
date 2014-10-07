1210a2ae6e71f6bdf22c951b9232c220b9e76095
Hetachi <krikus6@inbox.lv>
2014-10-07 19:35:22 +0300

    Fixed logout

--- resources/functions/logout.php
+++ resources/functions/logout.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-if (isset($_SESSION['myusername']) == FALSE) {
+if (isset($_SESSION['LoggedIn']) == FALSE) {
 header("location:../../index.php");
 }
 else {
 session_destroy();
+session_unset();
 header("location:../../index.php");
 }
 ?>
