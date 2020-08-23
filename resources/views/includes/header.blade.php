77f3c84ef2e881a304a56460c9e0b962d034b6f1
Solivar <solovjovs.ivars@gmail.com>
2020-08-23 16:21:31 +0300

    Add basic layout views

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,6 @@
+<div id="header">
+    <div id="site-branding">
+        <h1>E-REP</h1>
+        <h2>Your #1 reputation tracking tool</h2>
+    </div>
+</div>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    @include('includes.head')
+<body>
+    @include('includes.header')
+
+    <div id="container">
+        @yield('content')
+    </div>
+
+    @include('includes.footer')
+</body>
+</html>
