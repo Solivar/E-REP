14c58e0328dc8923be6a80b0f85aeb50479968c7
Solivar <solovjovs.ivars@gmail.com>
2020-10-03 15:19:06 +0300

    Add Votes table migration

---
+++ database/migrations/2020_09_25_151306_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('voter_id');
+            $table->foreign('voter_id')->references('id')->on('users');
+            $table->foreignId('addressee_id');
+            $table->foreign('addressee_id')->references('id')->on('users');
+            $table->text('description');
+            $table->tinyInteger('vote');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
